b45e23bde0a6d588e17e36ffdbb2d71ae5b7be07
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-20 02:29:56 +0300

    interface update

--- server/src/router/interface.go
+++ server/src/router/interface.go
@@ -1,6 +1,8 @@
 package routerHandler
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type PostData struct {
 	Message_ID int64          `json:"description"`
@@ -8,20 +10,33 @@ type PostData struct {
 	Message    sql.NullString `json:"message"`
 	Prev       sql.NullInt64  `json:"prev_message"`
 	Next       sql.NullInt64  `json:"next_message"`
-	Reward     sql.NullString   `json:"reward"`
+	Reward     sql.NullString `json:"reward"`
 }
 
 type PostDataConvert struct {
-	Message_ID int64  `json:"description"`
-	User       string `json:"user"`
-	Message    string `json:"message"`
-	Prev       int64  `json:"prev_message"`
-	Next       int64  `json:"next_message"`
+	Message_ID int64    `json:"description"`
+	User       string   `json:"user"`
+	Message    string   `json:"message"`
+	Prev       int64    `json:"prev_message"`
+	Next       int64    `json:"next_message"`
 	Reward     []Reward `json:"reward"`
 }
-
 
 type Reward struct {
 	Reward   string `json:"reward"`
 	AllCount int    `json:"all_count"`
+}
+
+type BodyDataLogin struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	Id        sql.NullInt64  `json:"id"`
+	Login     sql.NullString `json:"login"`
+	Password  sql.NullString `json:"password"`
+	Create    sql.NullString `json:"create_date"`
+	Confirmed sql.NullInt64  `json:"confirm"`
+	Mail      sql.NullString `json:"mail"`
 }
