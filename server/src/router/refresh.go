43b8bdb92d11c1654c94852db8613a219d48a8ab
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-20 02:30:17 +0300

    refresh add

---
+++ server/src/router/refresh.go
@@ -0,0 +1,24 @@
+package routerHandler
+
+import (
+	"net/http"
+
+	jwt_server "github.com/Cthulhu-tech/reditt_copy/tree/master/server/src/utils/jwt"
+)
+
+func refresh(w http.ResponseWriter, r *http.Request) {
+
+	var user = ""
+	var duration = 168
+
+	refreshToken, err := jwt_server.CreateJWT(duration)
+
+	if err != nil {
+		ErrorHandler(w, "Server Error", 500)
+		return
+	}
+
+	jwt_server.SetRefreshToken(w, r, duration, refreshToken)
+	jwt_server.SetAccessToken(w, r, 1, user)
+
+}
