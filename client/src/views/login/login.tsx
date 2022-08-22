5acf4894d2bd52904b3c644e29ef1cef788126e4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-22 06:59:13 +0300

    login and registration add

---
+++ client/src/views/login/login.tsx
@@ -0,0 +1,32 @@
+import { validate } from "../../utils/validator"
+import { useFetch } from "../../hook/useFetch"
+import { useForm } from "../../hook/useForm"
+import { useEffect } from 'react';
+
+export const Login = () => {
+
+    const responseData = () => {
+
+        FetchData('login')
+
+    }
+
+    const {load, data, errorResponse, FetchData} = useFetch();
+    const {handleInput, handleSubmit, error} = useForm(responseData, validate);
+
+    useEffect(() => {}, [error])
+
+    return <form onSubmit={handleSubmit}>
+        <input type="text" name="login" placeholder="login"
+            onChange={handleInput}
+        />
+        {error?.login && <p>{error.login}</p>}
+        <input type="text" name="password" placeholder="password"
+            onChange={handleInput}
+        />
+        {error?.password && <p>{error.password}</p>}
+        <button type="submit">login</button>
+    </form>
+
+
+}
