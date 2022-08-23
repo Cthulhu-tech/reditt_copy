ca3318374571bcea66984e38b096f55cddbe50cb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-23 11:55:31 +0300

    db update

--- server/database/reddit.sql
+++ server/database/reddit.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Авг 23 2022 г., 07:05
--- Версия сервера: 8.0.19
--- Версия PHP: 7.4.5
+-- Хост: localhost:3306
+-- Время создания: Авг 23 2022 г., 11:54
+-- Версия сервера: 8.0.30-0ubuntu0.20.04.2
+-- Версия PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +26,89 @@ DELIMITER $$
 --
 -- Процедуры
 --
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_create_post` (IN `user_id_value` INT(11), IN `reddit_group_value` INT(11), IN `title_value` CHAR(55), IN `description_value` VARCHAR(535))  NO SQL
+BEGIN
+	INSERT INTO post 
+    	(post.user_id, post.reddit_group_id, post.create_date, post.title, post.description)
+	VALUES 
+    	(user_id_value, reddit_group_value, now(),title_value, description_value);
+    INSERT INTO post_like (
+        post_like.user_id, post_like.post_title, post_like.value)
+    VALUES
+    	(user_id_value, title_value, 1);
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_dislike_message` (IN `user_id_value` INT(11), IN `messsage_id_value` INT(11))  NO SQL
+BEGIN
+
+ SET @USER_FIND = (SELECT case when COUNT(user.id) = 1 THEN 'FIND' ELSE 'ERROR' 
+                END USER_FIND FROM user WHERE user.id = user_id_value);
+
+     
+IF (@USER_FIND = 'FIND') THEN
+     
+     SET @OUTPUT = (SELECT case when COUNT(message_like.message_id) = 0 THEN 'OK' ELSE 'ERROR' 
+                    END OUTPUT FROM message_like WHERE 
+     message_like.user_id = user_id_value AND message_like.message_id = messsage_id_value);
+
+    IF (@OUTPUT = 'OK') THEN
+        INSERT INTO message_like
+        (message_like.message_id, 
+         message_like.user_id, 
+         message_like.value)
+        VALUES
+        (messsage_id_value, 
+         user_id_value,
+        -1);
+    ELSE
+        DELETE FROM message_like 
+        WHERE 
+        message_like.user_id = user_id_value
+        AND
+        message_like.message_id = messsage_id_value;
+    END IF;
+     
+END IF;
+     
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_dislike_post` (IN `user_id_value` INT(11), IN `post_title_value` CHAR(55))  NO SQL
+BEGIN
+
+ SET @USER_FIND = (SELECT case when COUNT(user.id) = 1 THEN 'FIND' ELSE 'ERROR' 
+                END USER_FIND FROM user WHERE user.id = user_id_value);
+
+     
+IF (@USER_FIND = 'FIND') THEN
+
+    SET @OUTPUT =
+        (SELECT case when  
+         COUNT(post_like.user_id) = 0 
+         THEN 'OK' ELSE 'ERROR' END OUTPUT
+        FROM post_like
+        WHERE post_like.user_id = user_id_value 
+        and post_like.post_title = post_title_value);
+
+        IF (@output = 'OK') THEN
+            INSERT INTO post_like
+            (post_like.post_title, 
+             post_like.user_id, 
+             post_like.value)
+            VALUES
+            (post_title_value, 
+             user_id_value,
+            -1);
+        ELSE
+            DELETE FROM post_like 
+            WHERE 
+            post_like.user_id = user_id_value
+            AND
+            post_like.post_title = post_title_value;
+        END IF;
+END IF;
+
+END$$
+
 CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_find_user_in_login` (IN `value_login` VARCHAR(255))  NO SQL
 BEGIN
 
@@ -42,7 +126,7 @@ BEGIN
 
 END$$
 
-CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `sp_get_all_message_in_post` (IN `value_post_id` INT(11))  NO SQL
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_get_all_message_in_post` (IN `value_post_id` INT(11))  NO SQL
 BEGIN
 
 	SELECT
@@ -70,7 +154,10 @@ BEGIN
        CONCAT('[',
        GROUP_CONCAT(DISTINCT message_reward.quantity),
          ']')
-        )	reward
+        )	reward,
+        
+       IF(COUNT(message_like.message_id) = 0, 0,        		
+       SUM(DISTINCT message_like.value)) message_value
         
     FROM post_message
     
@@ -113,6 +200,10 @@ BEGIN
     
     ON message_reward.message_id = post_message.id
     
+    LEFT JOIN message_like
+    
+    ON message_like.message_id = post_message.id
+    
     WHERE
     
     post_message.post_id = value_post_id
@@ -131,6 +222,194 @@ BEGIN
     
     ASC;
     
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_get_best_post` (IN `page` INT(11))  NO SQL
+BEGIN
+
+	SELECT
+        
+        GROUP_CONCAT(
+            DISTINCT 
+            (SELECT user.login 
+             FROM user 
+             WHERE 
+             user.id = post.user_id)) user,
+             
+       post.title title,
+       
+       post.description description,
+        
+
+        CONCAT('[',
+        GROUP_CONCAT(DISTINCT post_img.source),
+        ']') 
+        
+        img,
+        
+        CONCAT('[',
+        GROUP_CONCAT(DISTINCT post_video.video_sorce),
+        ']') 
+        
+        video,
+        
+       IF(
+       COUNT(post_reward.post_id) = 0, NULL,        		
+       CONCAT('[',
+       GROUP_CONCAT(DISTINCT post_reward.quantity),
+         ']')
+        )	reward,
+        
+       IF(COUNT(post_like.user_id) = 0, 0,        		
+       SUM(DISTINCT post_like.value)) post_value
+        
+    FROM post
+    
+    LEFT JOIN  post_video
+    
+    ON post_video.post_id = post.id
+    
+    LEFT JOIN post_img
+    
+    ON post_img.post_id = post.id
+    
+    LEFT JOIN post_like
+    
+    ON post.title = post_like.post_title
+    
+    LEFT JOIN
+    
+    (SELECT
+     
+     post_reward.post_id post_id,
+     
+     	JSON_OBJECT(
+         
+     		'all_count',
+            
+            COUNT(post_reward.post_id),
+         
+     		'reward',
+            
+             post_reward.reward
+            
+     )	quantity
+     
+     FROM post_reward 
+     
+     GROUP BY
+     
+     	post_reward.post_id, 
+     	
+     	post_reward.reward)
+    
+    	post_reward
+        
+	ON post_reward.post_id = post.id
+    
+    GROUP BY
+    
+    	post.id,
+        
+        post.user_id
+        
+    ORDER BY 
+    
+    	post.id,
+        
+        post.user_id
+    
+    ASC;
+    
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_get_like_message` (IN `messsage_id_value` INT(11))  NO SQL
+BEGIN
+	SELECT 
+    	IF(
+           COUNT(message_like.value) = 0, 
+           0, 
+           SUM(message_like.value)
+        ) value,
+        messsage_id_value id
+    FROM
+    	message_like
+    WHERE
+    	message_like.message_id = messsage_id_value;
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_like_message` (IN `user_id_value` INT(11), IN `messsage_id_value` INT(11))  NO SQL
+BEGIN
+
+ SET @USER_FIND = (SELECT case when COUNT(user.id) = 1 THEN 'FIND' ELSE 'ERROR' 
+                END USER_FIND FROM user WHERE user.id = user_id_value);
+
+     
+IF (@USER_FIND = 'FIND') THEN
+
+    SET @OUTPUT =
+        (SELECT case when  
+         COUNT(message_like.message_id) = 0 
+         THEN 'OK' ELSE 'ERROR' END OUTPUT
+        FROM message_like
+        WHERE message_like.user_id = user_id_value 
+        and message_like.message_id = messsage_id_value);
+
+        IF (@output = 'OK') THEN
+            INSERT INTO message_like
+            (message_like.message_id, 
+             message_like.user_id, 
+             message_like.value)
+            VALUES
+            (messsage_id_value, 
+             user_id_value,
+            1);
+        ELSE
+            DELETE FROM message_like 
+            WHERE 
+            message_like.user_id = user_id_value
+            AND
+            message_like.message_id = messsage_id_value;
+        END IF;
+END IF;
+
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_like_post` (IN `user_id_value` INT(11), IN `post_title_value` CHAR(55))  NO SQL
+BEGIN
+
+ SET @USER_FIND = (SELECT case when COUNT(user.id) = 1 THEN 'FIND' ELSE 'ERROR' 
+                END USER_FIND FROM user WHERE user.id = user_id_value);
+
+     
+IF (@USER_FIND = 'FIND') THEN
+
+    SET @OUTPUT =
+        (SELECT case when  
+         COUNT(post_like.user_id) = 0 
+         THEN 'OK' ELSE 'ERROR' END OUTPUT
+        FROM post_like
+        WHERE post_like.user_id = user_id_value 
+        and post_like.post_title = post_title_value);
+
+        IF (@output = 'OK') THEN
+            INSERT INTO post_like
+            (post_like.post_title, 
+             post_like.user_id, 
+             post_like.value)
+            VALUES
+            (post_title_value, 
+             user_id_value,
+            1);
+        ELSE
+            DELETE FROM post_like 
+            WHERE 
+            post_like.user_id = user_id_value
+            AND
+            post_like.post_title = post_title_value;
+        END IF;
+END IF;
+
 END$$
 
 CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `sp_save_or_delete_post_in_id` (IN `post_id` INT(11), IN `user_id` INT)  NO SQL
@@ -154,6 +433,47 @@ SET @OUTPUT =
         save_post.post = post_id
         AND
         save_post.user = user_id;
+    END IF;
+END$$
+
+CREATE DEFINER=`thrackerzod`@`localhost` PROCEDURE `sp_set_message_in_post` (IN `user_id_value` INT(11), IN `message_value` CHAR(55), IN `post_id_value` INT(11), IN `next_message_value` INT(11), IN `prev_message_value` INT(11))  NO SQL
+BEGIN
+	SET @OUTPUT =
+	(SELECT case when  
+     COUNT(*) = 1 
+     THEN 'OK' ELSE 'ERROR' END OUTPUT
+	FROM post
+ 	WHERE post.id = post_id_value);
+    
+    SET @NEXT_VALUE = next_message_value;
+    SET @PREV_VALUE = prev_message_value;
+    
+    IF (@output = 'OK') THEN
+        INSERT INTO post_message
+        (post_id, user_id, message)
+        VALUES
+        (post_id_value, user_id_value, message_value);
+        
+        IF (@NEXT_VALUE > 0) THEN
+        	INSERT INTO n_message
+            (n_message.message_id, n_message.n_message)
+            VALUES
+            ((SELECT LAST_INSERT_ID()), next_message_value);
+        END IF;
+        IF (@PREV_VALUE > 0) THEN
+            INSERT INTO p_message
+            (p_message.message_id, p_message.p_message)
+            VALUES
+            ((SELECT LAST_INSERT_ID()), prev_message_value);
+        END IF;
+    INSERT INTO message_like 
+        (message_like.message_id, 
+         message_like.user_id, 
+         message_like.value)
+    VALUES
+    	((SELECT LAST_INSERT_ID()),
+        user_id_value, 
+        1);
     END IF;
 END$$
 
@@ -191,6 +511,15 @@ CREATE TABLE `message_like` (
   `user_id` int NOT NULL,
   `value` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `message_like`
+--
+
+INSERT INTO `message_like` (`message_id`, `user_id`, `value`) VALUES
+(1, 1, -1),
+(3, 1, 1),
+(13, 1, -1);
 
 -- --------------------------------------------------------
 
@@ -252,7 +581,8 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `user_id`, `reddit_group_id`, `create_date`, `title`, `description`) VALUES
-(1, 1, 1, '2022-08-18 01:23:15', 'fake_reddit start', 'fake');
+(1, 1, 1, '2022-08-18 01:23:15', 'fake_reddit start', 'fake'),
+(3, 1, 1, '2022-08-23 04:47:39', 'test', 'test');
 
 -- --------------------------------------------------------
 
@@ -267,6 +597,15 @@ CREATE TABLE `post_img` (
   `source` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Дамп данных таблицы `post_img`
+--
+
+INSERT INTO `post_img` (`id`, `post_id`, `description`, `source`) VALUES
+(1, 1, 'post-fake.png', 'post-fake.png'),
+(2, 1, 'post-fake1.png', 'post-fake1.png'),
+(3, 1, 'post-fake2.png', 'post-fake2.png');
+
 -- --------------------------------------------------------
 
 --
@@ -275,7 +614,7 @@ CREATE TABLE `post_img` (
 
 CREATE TABLE `post_like` (
   `user_id` int NOT NULL,
-  `post_id` int NOT NULL,
+  `post_title` char(55) NOT NULL,
   `value` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -299,18 +638,32 @@ CREATE TABLE `post_message` (
 INSERT INTO `post_message` (`id`, `post_id`, `user_id`, `message`) VALUES
 (1, 1, 1, 'it\'s cool!'),
 (2, 1, 1, 'real:?'),
-(3, 1, 1, 'test');
+(3, 1, 1, 'test'),
+(5, 3, 1, 'hello!'),
+(6, 3, 1, 'hello!'),
+(7, 3, 1, 'work!'),
+(13, 3, 1, 'hello, it\'s work!');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `post _reward`
+-- Структура таблицы `post_reward`
 --
 
-CREATE TABLE `post _reward` (
+CREATE TABLE `post_reward` (
   `post_id` int NOT NULL,
   `reward` char(55) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `post_reward`
+--
+
+INSERT INTO `post_reward` (`post_id`, `reward`) VALUES
+(1, 'super'),
+(1, 'cool'),
+(1, 'cool'),
+(3, 'super');
 
 -- --------------------------------------------------------
 
@@ -323,6 +676,14 @@ CREATE TABLE `post_video` (
   `video_sorce` char(255) NOT NULL,
   `description` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `post_video`
+--
+
+INSERT INTO `post_video` (`post_id`, `video_sorce`, `description`) VALUES
+(1, 'video.mp4', 'video.mp4'),
+(3, 'video1.mp4', 'video1.mp4');
 
 -- --------------------------------------------------------
 
@@ -342,7 +703,8 @@ CREATE TABLE `p_message` (
 
 INSERT INTO `p_message` (`id`, `message_id`, `p_message`) VALUES
 (1, 2, 1),
-(2, 3, 1);
+(2, 3, 1),
+(3, 7, 6);
 
 -- --------------------------------------------------------
 
@@ -463,7 +825,8 @@ ALTER TABLE `post`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD KEY `user_login` (`user_id`),
-  ADD KEY `reddit_group` (`reddit_group_id`);
+  ADD KEY `reddit_group` (`reddit_group_id`),
+  ADD KEY `title` (`title`);
 
 --
 -- Индексы таблицы `post_img`
@@ -478,7 +841,7 @@ ALTER TABLE `post_img`
 --
 ALTER TABLE `post_like`
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `post_id` (`post_id`);
+  ADD KEY `post_title` (`post_title`);
 
 --
 -- Индексы таблицы `post_message`
@@ -490,9 +853,9 @@ ALTER TABLE `post_message`
   ADD KEY `user_login` (`user_id`);
 
 --
--- Индексы таблицы `post _reward`
+-- Индексы таблицы `post_reward`
 --
-ALTER TABLE `post _reward`
+ALTER TABLE `post_reward`
   ADD KEY `post_id` (`post_id`),
   ADD KEY `reward` (`reward`);
 
@@ -571,25 +934,25 @@ ALTER TABLE `n_message`
 -- AUTO_INCREMENT для таблицы `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `post_img`
 --
 ALTER TABLE `post_img`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `post_message`
 --
 ALTER TABLE `post_message`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT для таблицы `p_message`
 --
 ALTER TABLE `p_message`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `rewards`
@@ -657,8 +1020,8 @@ ALTER TABLE `post_img`
 -- Ограничения внешнего ключа таблицы `post_like`
 --
 ALTER TABLE `post_like`
-  ADD CONSTRAINT `post_like_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `post_like_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `post_like_ibfk_3` FOREIGN KEY (`post_title`) REFERENCES `post` (`title`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `post_like_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `post_message`
@@ -668,11 +1031,11 @@ ALTER TABLE `post_message`
   ADD CONSTRAINT `post_message_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `post _reward`
+-- Ограничения внешнего ключа таблицы `post_reward`
 --
-ALTER TABLE `post _reward`
-  ADD CONSTRAINT `post _reward_ibfk_1` FOREIGN KEY (`reward`) REFERENCES `rewards` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `post _reward_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `post_reward`
+  ADD CONSTRAINT `post_reward_ibfk_1` FOREIGN KEY (`reward`) REFERENCES `rewards` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_reward_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `post_video`
