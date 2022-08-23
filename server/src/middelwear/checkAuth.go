d596e3c6b7c808435bde6ff7f233557e80a6ac17
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-08-24 03:05:51 +0400

    check auth add

--- server/src/middelwear/checkAuth.go
+++ server/src/middelwear/checkAuth.go
@@ -1,17 +1,42 @@
 package middelwear
 
 import (
-	"log"
 	"net/http"
+
+	errorHTTP "github.com/Cthulhu-tech/reditt_copy/tree/master/server/src/utils/errorHandler"
+	jwt_server "github.com/Cthulhu-tech/reditt_copy/tree/master/server/src/utils/jwt"
+	"github.com/gorilla/context"
 )
 
 func CheckAuth(next http.Handler) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		log.Printf("userHandler")
+		token, err := jwt_server.GetBearerToken(r)
 
-		next.ServeHTTP(w, r)
+		if err != nil {
+
+			errorHTTP.ErrorHandler(w, "Need authorization", 403)
+
+			return
+
+		}
+
+		check, name := jwt_server.CheckToken(r, token)
+
+		if check {
+
+			context.Set(r, "username", name)
+
+			next.ServeHTTP(w, r)
+
+		} else {
+
+			errorHTTP.ErrorHandler(w, "Need authorization", 403)
+
+			return
+
+		}
 
 	}
 
